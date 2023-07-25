8878d7a61efde0fc41fbcc40d759f2e75685b6ac
Devashree Gosavi <35646522+devashreegosavi@users.noreply.github.com>
2023-07-25 13:37:34 +0530

    25 july 2023
    0137pm

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,7 +12,8 @@ export class EditServerComponent implements OnInit{
   serverName ='';
   serverStatus ='';
   allowEdit = false;
-  constructor(private serversService: ServersService,private route :ActivatedRoute){
+  changesSaved = false;
+  constructor(private serversService: ServersService,private route :ActivatedRoute, private router : Router){
 
   }
 
@@ -25,12 +26,15 @@ export class EditServerComponent implements OnInit{
       }
     );
     this.route.fragment.subscribe();
-    this.server=this.serversService.getServer(1)!;
+    const id = +this.route.snapshot.params['id'];
+    this.server=this.serversService.getServer(id)!;
     this.serverName=this.server.name;
     this.serverStatus=this.server.status;
 
   }
    onupdateServer(){
     this.serversService.updateServer(this.server.id, {name : this.serverName, status :this.serverStatus});
+    this.changesSaved = true;
+    this.router.navigate(['../'], {relativeTo: this.route});
    }
 }
